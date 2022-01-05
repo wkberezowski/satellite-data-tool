0acb4150ec2c008cf70e193ce9a727e01fc2eeb8
Wojciech Berezowski <61703808+wkberezowski@users.noreply.github.com>
2022-01-05 16:02:51 +0100

    added configuring dataviewer by user

--- user_interface.py
+++ user_interface.py
@@ -1,3 +1,4 @@
+from os import stat
 from tkinter import *
 from tkinter import filedialog
 from data_viewer import *
@@ -9,35 +10,18 @@ import numpy as np
 
 # IMPORTING HDF5 FILE AND DISPLAYING ITS CONTENT
 
-
-def display_hdf(filename):
-    dataframe = h5py.File(filename, 'r')
-    grid = dataframe['Grid']
-    grid_keys = list(grid.keys())
-    sizes = []
-
-    for key in grid_keys:
-        sizes.append(grid[key].size)
-
-    return grid_keys, sizes
-
-
-def display_netcdf(filename):
-    dataframe = netCDF4.Dataset(
-        filename, 'r', format='NETCDF4')
-    vars = list(dataframe.variables)
-    sizes = []
-
-    for var in vars:
-        sizes.append(dataframe[var].size)
-
-    return vars, sizes
+hdf_names = ['.h5', '.hdf5']
+netcdf_names = ['.nc4', '.nc']
 
 
 def open():
     global filename
     global add_to_list
-    filetypes = (('HDF5 files', '*.HDF5'), ('netCDF files', '*.nc4'))
+    filetypes = [('HDF5 files', '*.HDF5'),
+                 ('HDF5 files', '*.h5'),
+                 ('netCDF files', '*.nc4'),
+                 ('netCDF files', '*.nc'),
+                 ]
     filename = filedialog.askopenfilename(
         initialdir='./data', title='Select a file', filetypes=filetypes)
 
@@ -61,9 +45,9 @@ def open():
                            background=primary_color)
         size_label.grid(row=1, column=2, pady=2, sticky='nsew')
 
-        if filename[-4:].lower() == 'hdf5':
+        if any(substring in filename.lower() for substring in hdf_names):
             vars, sizes = display_hdf(filename)
-        elif filename[-3:-1].lower() == 'nc':
+        elif any(substring in filename.lower() for substring in netcdf_names):
             vars, sizes = display_netcdf(filename)
 
         checkbuttons = {}
@@ -96,18 +80,45 @@ def open():
 
         def show_list():
             value_list = add_to_list()
-            list_label = Label(display_frame, text='{}'.format(value_list),
-                               background=primary_color)
-            list_label.grid(columnspan=3, pady=2, sticky='nsew')
-
-            btn_open_as_csv = Button(display_frame, text="Open As CSV", command=open_in_dataviewer,
-                                     bg=btns_color, width=50)
-
-            btn_open_as_csv.grid(columnspan=3, pady=10, sticky='ns')
+            list_label.configure(text='{}'.format(value_list))
+            btn_open_as_csv.configure(state=NORMAL)
 
         add_to_list_btn = Button(display_frame, text='Add To List',
                                  background=btns_color, width=50, command=show_list)
         add_to_list_btn.grid(columnspan=3, pady=10, sticky='ns')
+
+        list_label = Label(display_frame,
+                           background=primary_color)
+        list_label.grid(columnspan=3, pady=2, sticky='nsew')
+
+        btn_open_as_csv = Button(display_frame, text="Open As CSV", command=open_in_dataviewer,
+                                 bg=btns_color, width=50, state=DISABLED)
+
+        btn_open_as_csv.grid(columnspan=3, pady=10, sticky='ns')
+
+
+def display_hdf(filename):
+    dataframe = h5py.File(filename, 'r')
+    grid = dataframe['Grid']
+    grid_keys = list(grid.keys())
+    sizes = []
+
+    for key in grid_keys:
+        sizes.append(grid[key].size)
+
+    return grid_keys, sizes
+
+
+def display_netcdf(filename):
+    dataframe = netCDF4.Dataset(
+        filename, 'r', format='NETCDF4')
+    vars = list(dataframe.variables)
+    sizes = []
+
+    for var in vars:
+        sizes.append(dataframe[var].size)
+
+    return vars, sizes
 
 
 # OPENING DATAVIEWER
@@ -115,40 +126,28 @@ def open():
 
 def open_in_dataviewer():
     list_of_values = add_to_list()
-    print(list_of_values)
 
     #  HANDLING HDF FILES
 
-    if filename[-4:].lower() == 'hdf5':
-        dataframe = h5py.File(filename, 'r')
+    if any(substring in filename.lower() for substring in hdf_names):
 
-        grid = dataframe['Grid']
+        dataset = h5py.File(filename, 'r')
 
-        lon_values = list(grid['lon'])
-        lat_values = list(grid['lat'])
-        percip_values = np.array(list(grid['precipitation'])).flatten()
+        grid = dataset['Grid']
 
-        dataframe = pd.DataFrame({'lon': lon_values[:1000],
-                                  'lat': lat_values[:1000],
-                                  'percip': percip_values[:1000]
-                                  })
+        dataframe = pd.DataFrame(columns=list_of_values)
 
-        dataframe.columns = [grid['lon'].attrs['standard_name'].decode() +
-                             ' (' + grid['lon'].attrs['units'].decode() + ')',
-                             grid['lat'].attrs['standard_name'].decode(
-        ) + ' (' + grid['lat'].attrs['units'].decode() + ')',
-            'Precipitation (' +
-            grid['precipitation'].attrs['units'].decode() + ')'
-        ]
-
-        dataframe['Precipitation (mm/hr)'] = dataframe['Precipitation (mm/hr)'].mask(
-            dataframe['Precipitation (mm/hr)'] == -9999.900391, 0)
+        for i in range(len(list_of_values)):
+            this_column = dataframe.columns[i]
+            dataframe[this_column] = np.array(
+                list(grid['{}'.format(list_of_values[i])])).flatten()[:1000]
 
         dataviewer(dataframe)
 
     # HANDLING NETCDF FILES
 
-    elif filename[-3:-1].lower() == 'nc':
+    elif filename[-3].lower() in 'nc':
+        print(filename[:-3])
         # READ DATASET
         dataset = netCDF4.Dataset(
             filename, 'r', format='NETCDF4')
@@ -164,10 +163,15 @@ def open_in_dataviewer():
 
         dataframe = pd.DataFrame({'lon': lon_values[:1000],
                                   'lat': lat_values[:1000],
-                                  'prcp': percip_values[:1000]})
+                                 'prcp': percip_values[:1000]})
 
-        dataframe.columns = ['{} in {}'.format(dataset['lon'].standard_name, dataset['lon'].units), '{} in {}'.format(
-            dataset['lat'].standard_name, dataset['lat'].units), '{} in {}'.format(dataset['prcp'].long_name, dataset['prcp'].units)]
+        dataframe.columns = [
+            '{} in {}'.format(
+                dataset['lon'].standard_name, dataset['lon'].units),
+            '{} in {}'.format(
+                dataset['lat'].standard_name, dataset['lat'].units),
+            '{} in {}'.format(dataset['prcp'].long_name, dataset['prcp'].units)
+        ]
 
         dataviewer(dataframe)
 
@@ -219,11 +223,11 @@ btns_frame.columnconfigure(0, weight=1)
 
 btn_open = Button(btns_frame, text="Open",
                   command=open, bg=btns_color)
-btn_open.grid(row=0, column=0, sticky='nsew', padx=10, pady=10)
+btn_open.grid(row=0, column=0, sticky='nsew', padx=10, pady=5)
 
 btn_clear = Button(btns_frame, text="Clear All",
                    command=clear_all, bg=btns_color)
-btn_clear.grid(row=1, column=0, sticky='nsew', padx=10, pady=10)
+btn_clear.grid(row=1, column=0, sticky='nsew', padx=10, pady=5)
 
 
 # SETTING UP LABELS FOR DISPLAYING THE CONTNET OF THE FILE
